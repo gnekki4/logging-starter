54feb8a95882d41b7c371b3576022ee1bdd9d455
Pechkov Nikita <n.pechkov@hostco.ru>
2024-11-16 14:17:20 +0500

    initial commit

---
+++ src/main/java/ru/gnekki4/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -0,0 +1,8 @@
+package ru.gnekki4.loggingstarter;
+
+public class LoggingStarterAutoConfiguration {
+
+  public static void println(String str) {
+    System.out.println("Вызвано из Gradle библиотеки: " + str);
+  }
+}
